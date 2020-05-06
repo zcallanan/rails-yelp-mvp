d3d2952b7bb8ef37b7b89a983e8ad66a517adfac
Zach Callanan <caveofmonsters@gmail.com>
2020-05-06 12:47:50 +0200

    seed for models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Review.destroy_all
+
+array = %w[chinese italian japanese french belgian]
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: array.sample
+  )
+  restaurant.save
+end
+
+restaurants = Restaurant.all
+
+10.times do
+  restaurant = restaurants.sample
+  review = Review.new(
+    content: Faker::Restaurant.review,
+    rating: (0..5).to_a.sample,
+    restaurant_id: restaurant.id
+  )
+  review.save
+end
