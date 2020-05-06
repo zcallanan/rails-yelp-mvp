9868ad8c3803f5ae15201a52b7ed995cc754b967
Zach Callanan <caveofmonsters@gmail.com>
2020-05-06 11:59:12 +0200

    review model updated

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, :rating, presence: true
-  validates :rating, inclusion: { in: (0..5) }
+  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
